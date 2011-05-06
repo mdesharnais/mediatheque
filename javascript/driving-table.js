74a838c588f14a78a7e29716e1a5931d4d5e6547
Martin Desharnais <martin.desharnais@gmail.com>
2011-05-05 22:06:44 -0400

    Convertit le code javascript du module de gestion des tables de pilotages en code jQuery.
    Ajoute le concept de row_sate afin de connaître l'état de chaque
    enregistrement de la table de pilotage.

--- javascript/driving-table.js
+++ javascript/driving-table.js
@@ -1,7 +1,15 @@
 $(document).ready(function() { 
 	initDrivingTable();
 
-	$("table").tablesorter({
+	$('form#driving-table tbody :input').change(function() {
+		var $tr = $(this).closest('tr');
+		var $input = $tr.find('td[headers=col_row_state] input');
+
+		if($input.val() != 2)
+			$input.val(4);
+	});
+
+	$('table').tablesorter({
 		sortList: [[1,0]],
 		textExtraction: extractInputValue
 	}); 
@@ -11,8 +19,61 @@ var extractInputValue = function(node) {
 	return $(node).find(':input').val();
 }
 
+function addRow() {
+	var $newRow = $(this).closest('tr').clone();
+	var $button = $newRow.find('td[headers="col_add_remove_buttons"] input[type=button]');
+
+	$newRow.removeAttr('id');
+	$button.click(removeRow);
+	$button.attr('value', 'Supprimer');
+
+	$('form#driving-table table tbody').append($newRow);
+
+	// Reset new row values
+	var $columns = $(this).closest('tr').find('td:not([headers=col_row_state]):not([headers=col_add_remove_buttons])');
+	$columns.find(':input').each(function() {
+		switch(this.type) {
+			case 'password':
+			case 'select-multiple':
+			case 'select-one':
+			case 'text':
+			case 'textarea':
+				$(this).val('');
+				break;
+			case 'checkbox':
+			case 'radio':
+				this.checked = false;
+		}
+	});
+}
+
+function removeRow() {
+	var $tr = $(this).closest('tr');
+	var $input = $tr.find('td[headers=col_row_state] input');
+
+	//alert($input.val());
+
+	switch($input.val()) {
+		case '1':
+		case '4':
+			$input.val('3');
+			break;
+		case '2':
+			$tr.remove();
+	}
+}
+
 function initDrivingTable()
 {
+	var $table = $('form#driving-table table');
+	$table.find('thead tr:first-child').append('<th id="col_add_remove_buttons">Ajouter/Supprimer</th>');
+	$table.find('thead tr#new-row').append('<td headers="col_add_remove_buttons"><input type="button" value="Ajouter"></td>');
+	$table.find('tbody tr').append('<td headers="col_add_remove_buttons"><input type="button" value="Supprimer"></td>');
+
+	$table.find('thead tr td[headers=col_add_remove_buttons] input[type=button]').click(addRow);
+	$table.find('tbody tr td[headers=col_add_remove_buttons] input[type=button]').click(removeRow);
+
+	/*
 	var drivingTable = document.getElementById('driving-table');
 
 	// Add "delete" buttons
@@ -46,8 +107,10 @@ function initDrivingTable()
 
 	td.appendChild(addButton);
 	tr.appendChild(td);
+	*/
 }
 
+/*
 function addRow(e)
 {
 	var drivingTable = document.getElementById('driving-table');
@@ -70,7 +133,7 @@ function addRow(e)
 	newRow.removeChild(newRow.lastElementChild);
 	newRow.appendChild(deleteButtonTd);
 
-	newRow.id = null;
+	newRow.removeAttribute('id');
 
 	tbody.appendChild(newRow);
 
@@ -88,4 +151,6 @@ function deleteRow(e)
 	var tr = td.parentNode;
 	var tbody = tr.parentNode;
 	tbody.removeChild(tr);
+	$('table').trigger('update');
 }
+*/
