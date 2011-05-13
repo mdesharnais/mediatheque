8979ac10a87363587c2514b80a3f6f04f2028ddf
Marc-Andre Destrempes <xeph_555@hotmail.com>
2011-05-13 08:52:27 -0400

    Correction du bug ou le bouton Afficher ne faisait absoluement rien.

--- javascript/generateEmprunt.js
+++ javascript/generateEmprunt.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 	$('form#emprunt input#utilisateurID').after('<input type="button" value="Afficher" class="foo">');
-	$('form#emprunt input[type="button"].foo').click(queryUserInfo());
-});
-
-function queryUserInfo()
-{
-	$.ajax({
-		type: "GET",
-		url: "utilisateurs.xml",
-		dataType: "xml",
-		success: function(xml) {
-			$(xml).find('utilisateur').each(function() {
-				var $userInfo = $('<div id="userInfo"></div>');
-				$userInfo.append($(this).attr('matricule') + '<br/>' + $(this).attr('prenom') + ' ' + $(this).attr('nom') + '<br/>' + $(this).attr('telephone') + '<br/>' + $(this).attr('email'));
-				$('form#emprunt input[type="button"].foo').after($userInfo);
-			});
-		}
+	$('form#emprunt input[type="button"].foo').click(function() {
+		$.ajax({
+			type: "GET",
+			url: "utilisateurs.xml",
+			dataType: "xml",
+			success: function(xml) {
+				$(xml).find('utilisateur').each(function() {
+					var $userInfo = $('<div id="userInfo"></div>');
+					if ($('form#emprunt input[type="number"]#utilisateurID').val() == $(this).attr('matricule'))
+					{
+						$userInfo.append($(this).attr('matricule') + '<br/>' + $(this).attr('prenom') + ' ' + $(this).attr('nom') + '<br/>' + $(this).attr('telephone') + '<br/>' + $(this).attr('email'));
+						$('form#emprunt input[type="button"].foo').after($userInfo);
+					}
+				});
+			}
+		});
 	});
-}
+});
