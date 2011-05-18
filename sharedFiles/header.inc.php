2dde9b7352265ec3fd9449b1fea286c2cc748667
Martin Desharnais <martin.desharnais@gmail.com>
2011-05-17 22:29:00 -0400

    Diminue légèrement la taille du nom du site dans l'en-tête.
    Modifie la disposition des éléments de l'en-tête lorsque la fenêtre
    atteint une taille de moins de 640x480 afin qu'ils se gènent moins les
    un les autres.

--- sharedFiles/header.inc.php
+++ sharedFiles/header.inc.php
@@ -6,7 +6,7 @@
 	<h1><a href="index.php" title="Retour à l'accueil">Carcajou</a></h1>
 	<img alt="Logo du Cégep de Trois-Rivières" src="images/logo.png">
 	<form>
-		<input type="search" size="60" placeholder="Entrez votre recherche">
+		<input type="search" placeholder="Entrez votre recherche">
 		<button type="submit"><img src="images/icons/search32x32.png" alt="Rechercher"></button>
 		<p id="moresearches"><a href="advancedSearch.php">Recherche avancée</a></p>
 	</form>
