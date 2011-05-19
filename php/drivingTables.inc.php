3bba2be7a1ff9fcaf192e62fff3c47e05f0c4711
Martin Desharnais <martin.desharnais@gmail.com>
2011-05-19 11:26:26 -0400

    Modification de la fonction printDrivingTable afin que celle-ci génère la table à partir des colonnes et des enregistrements de la table de la base de données.

--- php/drivingTables.inc.php
+++ php/drivingTables.inc.php
@@ -1,6 +1,13 @@
 <?php
+require_once('Application.class.php');
+
 function printDrivingTable($tableName)
 {
+	global $application;
+
+	foreach($application->database->query("SHOW FULL COLUMNS FROM $tableName") as $row)
+		$columns[$row['Field']] = $row;
+
 	/*
 	row_state:
 	1 = Unchanged
@@ -8,51 +15,69 @@ function printDrivingTable($tableName)
 	3 = Deleted
 	4 = Modified
 	*/
-	echo '<form id="driving-table">
-				<table>
-					<caption>'.$tableName.'</caption>
-					<thead>
-						<tr>
-							<th id="col_row_state">Row state</th>
-							<th id="col_id">ID</th>
-							<th id="col_nom">Nom</th>
-							<th id="col_description">Description</th>
-							<th id="col_inactif">Inactif</th>
-						</tr>
-						<tr id="new-row">
-							<td headers="col_row_state"><input type="number" id="row_state" name="row_state" value="2" required disabled></td>
-							<td headers="col_id"><input type="number" id="id" name="id" disabled></td>
-							<td headers="col_nom"><input type="text" id="nom" name="nom"></td>
-							<td headers="col_description"><input type="text" id="description" name="description"></td>
-							<td headers="col_inactif"><input type="checkbox" id="inactif" name="inactif"></td>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td headers="col_row_state"><input type="number" id="row_state1" name="row_state1" value="1" required disabled></td>
-							<td headers="col_id"><input type="number" id="id1" name="id1" value="1" disabled></td>
-							<td headers="col_nom"><input type="text" id="nom1" name="nom1" value="AAA" required></td>
-							<td headers="col_description"><input type="text" id="description1" name="description1" value="Lorem ipsum dolor sit amet."></td>
-							<td headers="col_inactif"><input type="checkbox" id="inactif1" name="inactif1"></td>
-						</tr>
-						<tr>
-							<td headers="col_row_state"><input type="number" id="row_state1" name="row_state1" value="1" required disabled></td>
-							<td headers="col_id"><input type="number" id="id2" name="id2" value="2" disabled></td>
-							<td headers="col_nom"><input type="text" id="nom2" name="nom2" value="DDD" required></td>
-							<td headers="col_description"><input type="text" id="description2" name="description2"></td>
-							<td headers="col_inactif"><input type="checkbox" id="inactif2" name="inactif2"></td>
-						</tr>
-						<tr>
-							<td headers="col_row_state"><input type="number" id="row_state1" name="row_state1" value="1" required disabled></td>
-							<td headers="col_id"><input type="number" id="id3" name="id3" value="3" disabled></td>
-							<td headers="col_nom"><input type="text" id="nom3" name="nom3" value="CCC" required></td>
-							<td headers="col_description"><input type="text" id="description3" name="description3" value="Consectetur adispsing elit."></td>
-							<td headers="col_inactif"><input type="checkbox" id="inactif3" name="inactif3"></td>
-						</tr>
-					</tbody>
-				</table>
-				<button type="submit">Enregistrer</button>
-				<button type="reset">Annuler</button>
-			</form>';
+
+	echo '<form id="driving-table" method="POST" action="php/saveDrivingTable.php">';
+	echo '	<table>';
+	echo '		<caption>'.$tableName.'</caption>';
+	echo '		<thead>';
+	echo '			<tr>';
+	echo '				<th id="col_row_state">Row state</th>';
+
+	foreach($columns as $column)
+		echo '<th id="col_'.$column['Field'].'">'.$column['Comment'].'</th>';
+
+	echo '			</tr>';
+	echo '			<tr id="new-row">';
+	echo '				<td headers="col_row_state"><input type="number" id="row_state" name="row_state" value="2" required disabled></td>';
+
+	foreach($columns as $column)
+		echo '<td headers="col_'.$column['Field'].'"><input type="text" id="'.$column['Field'].'" name="'.$column['Field'].'"></td>';
+
+	echo '				</tr>';
+	echo '			</thead>';
+	echo '			<tbody>';
+
+	foreach($application->database->query("SELECT * FROM $tableName") as $row)
+	{
+		echo '				<tr>';
+		echo '					<td headers="col_row_state"><input type="number" id="row_state1" name="row_state1" value="1" required disabled></td>';
+
+		foreach($columns as $column)
+		{
+			echo '<td headers="col_'.$column['Field'].'">';
+			generateField($column, $row[$column['Field']]);
+			echo '</td>';
+		}
+
+		echo '				</tr>';
+	}
+
+	echo '			</tbody>';
+	echo '	</table>';
+	echo '	<button type="submit">Enregistrer</button>';
+	echo '	<button type="reset">Annuler</button>';
+	echo '</form>';
+}
+
+function generateField($column, $value)
+{
+	switch(substr($column['Type'], 0, strpos($column['Type'], '(')))
+	{
+	case 'tinyint':
+	case 'smallint':
+	case 'mediumint':
+	case 'int':
+	case 'bigint':
+	case 'float':
+	case 'double':
+	case 'decimal':
+	case 'numeric':
+		$type = 'number';
+		break;
+
+	default:
+		$type = 'text';
+	}
+	echo '	<input type="'.$type.'" id="'.$column['Field'].'" name="'.$column['Field'].'" value="'.$value.'">';
 }
 ?>
