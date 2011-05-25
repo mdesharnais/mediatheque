e440e8da7e53df6cff75e29fa0906ba693c7e5fe
Samuel Milette-Lacombe <samulacombe@gmail.com>
2011-05-25 17:38:54 -0400

    Continuation de l'affichage des résultats. Optimisation de la requête sql. La requête est maintenant faites avec des jointures JOIN. Affichage de l'image du type de média lorsque le média n'a pas d'image. Règle le problème des bordure de TR qui ne s'affichait pas. La solution est de mettre des bordures aux TD.

--- php/search-results.inc.php
+++ php/search-results.inc.php
@@ -8,57 +8,54 @@ function printSearchResults($sqlQuery)
 	$query = $application->database->prepare($sqlQuery);
 	$query->execute();
 	
-	echo '<table>';
+	echo '<table id="resultTable">';
 	
 	foreach($query as $row)
 	{
 		echo '<tr class="mediaRowResult">';
 		
 		echo '<td class="mediaPicture">';
-		if ($row['image']<>"")
+		if (!empty($row['image']))
 		{
 			echo '<p>';
-			echo '<img class="resultPicture" src="images/medias/'.$row['image'].'" alt="Illustration du média: '.$row['titre'].'"/>';
+			echo '<img class="resultPicture" src="images/medias/'.$row['image'].' " alt="Illustration du média: '.$row['titre'].'"/>';
 			echo '</p>';
 		}
 		else
 		{
-			//echo '<p>';
-			//echo '<img src="'.$row['image'].'" alt="Illustration du média: '.$row['titre'].'"/>';
-			//echo '</p>';
+			echo '<p>';
+			echo '<img class="mediaTypePicture" src="images/typesMedia/'.$row['imageCategorie'].'" alt="'.$row['nomCategorie'].'"/>';
+			echo '</p>';
 		}
 		echo '</div>';//mediaPicture
 		
 		echo '<td class="mediaInformations">';
 		echo '<h4 class="mediaTitle"><a href="media.php">'.$row['titre'].'</a></h4>';
 		echo '<p>';
-		echo '<span class="label">Éditeur: </span><span class="value">'.$application->getDrivingTableElementName('maisons_edition',$row['maison_editionID']).
-		', '.$row['annee_publication'].'</span>';
+		echo '<span class="label">Éditeur: </span><span class="value">'.$row['nomMaisonEdition'].', '.$row['annee_publication'].'</span>';
 		echo '</p>';
 		echo '<p>';
-		$categorieMediaID = $application->getFurtherRowID('supports','categorieMediaID',$row['supportID']);
-		echo '<span class="label">Catégorie: </span><span class="value">'.$application->getDrivingTableElementName('categoriesMedia',$categorieMediaID).' - '.
-		$application->getDrivingTableElementName('supports',$row['supportID']).'</span>';
+		echo '<span class="label">Catégorie: </span><span class="value">'.$row['nomCategorie'].' - '.$row['nomSupport'].'</span>';
 		echo '</p>';
 		
-		if (!is_null($row['genreID']))
+		if (!empty($row['nomGenre']))
 		{
 			echo '<p>';
-			echo '<span class="label">Genre: </span><span class="value">'.$application->getDrivingTableElementName('genres',$row['genreID']).'</span>';
+			echo '<span class="label">Genre: </span><span class="value">'.$row['nomGenre'].'</span>';
 			echo '</p>';
 		}
 		
-		if ($row['notes']<>"")
+		if (!empty($row['notes']))
 		{
 			echo '<p>';
 			echo '<span class="label">Notes: </span><span class="value">'.$row['notes'].'</span>';
 			echo '</p>';
 		}
 		
-		if (!is_null($row['artisteID']))
+		if (!empty($row['nomArtiste']))
 		{
 			echo '<p>';
-			echo '<span class="label">Artiste principal:</span><span class="value">'.$application->getDrivingTableElementName('artistes',$row['artisteID']).'</span>';
+			echo '<span class="label">Artiste principal:</span><span class="value">'.$row['nomArtiste'].'</span>';
 			echo '</p>';
 		}
 		
