c31f37436c05248733f5b5098a4e5d04758a8667
Martin Desharnais <martin.desharnais@gmail.com>
2011-05-27 09:09:45 -0400

    Suppression de deux méthodes qui n'étaient utilisées nul part dans le projet et qui n'avaient plus d'utilité.

--- php/Application.class.php
+++ php/Application.class.php
@@ -37,39 +37,6 @@ class Application
 			echo $e->getMessage();
 		}
 	}
-	
-	/*Fonction qui retourne le champ "nom" de l'enregistrement d'une table de pilotage à
-	partir de son ID*/
-	public function getDrivingTableElementName($tableName,$ID)
-	{
-			
-						
-		foreach($this->database->query("SELECT nom from ".$tableName." WHERE ID=".$ID) as $row)
-			$nom = $row['nom'];
-		
-		if (isset($nom))
-			return $nom;
-		else
-			return 'Non définie';
-		
-	}
-	
-	/*Fonction qui retourne un champ d'identification unique (ex: categorieMediaID)
-	dans un enregistrement d'une table	Exemple d'utilisation: Je suis actuellement dans des enregistrements de la 
-	table "médias" et je veux avoir le ID de la catégorie de média utilisée par le support de média courant.
-	 getFurtherRowID('supports','categorieMediaID',$row['supportID']);
-	*/
-	public function getFurtherRowID($tableName,$fieldName,$ID)
-	{
-									
-		foreach($this->database->query("SELECT ".$fieldName. " from ".$tableName." WHERE ID=".$ID) as $row)
-			$otherID = $row[$fieldName];
-		
-		if (isset($otherID))
-			return $otherID;
-		else
-			return 0;
-	}
 }
 
 $application = new Application();
