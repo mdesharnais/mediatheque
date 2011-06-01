efe5546735aff14d03531d587447fb113c5e504f
Martin Desharnais <martin.desharnais@gmail.com>
2011-06-01 19:32:50 -0400

    Modification de la page affichant les emprunts en cours de l'utilisateur courant.

--- my_reservation.php
+++ my_reservation.php
@@ -8,18 +8,65 @@
 		<title>Médiatech du département de musique du cégep de Trois-Rivières</title>
 		<?php include('sharedFiles/style.inc.php'); ?>
 		<?php include('sharedFiles/javascript.inc.php'); ?>
-		<style>.tooltip{display:none;background:url(http://static.flowplayer.org/tools/img/tooltip/black_arrow_big.png);height:163px;padding:40px 30px 10px 30px;width:310px;font-size:11px;color:#fff;}#outerDiv{min-width: 200px;}div.autosize{display:table;height:1px;}div.autosize>div{display:table-cell;}</style>
-		<script src='http://cdn.jquerytools.org/1.2.5/full/jquery.tools.min.js'></script>
 	</head>
 	<body>
 		<?php require('sharedFiles/header.inc.php'); ?>
 		<div id="content">
 			<div>
-			<img src="images/reservation.png" />
-				<h1>Mes Réservations</h1><br>
-					<?php
-						include("php/myReservations.php");
-					?>
+				<?php
+				if($application->currentUser->isVisitor())
+				{
+					echo '<h1>Erreur</h1>';
+					echo '<p>Vous devez être connectés pour consulter vos emprunts en cours.<p>';
+				}
+				else
+				{
+					echo '<h1>Mes réservations</h1>';
+					$query = $application->database->prepare('
+						SELECT medias.ID, 
+							medias.titre, 
+							emprunts.date_reservation
+						FROM medias 
+							INNER JOIN emprunts ON medias.ID = emprunts.mediaID 
+						WHERE emprunts.utilisateurID = ? AND emprunts.date_emprunt IS NULL AND emprunts.date_reservation IS NOT NULL');
+
+					$query->execute(array($application->currentUser->getID()));
+
+					echo '<table>';
+					echo '	<thead>';
+					echo '		<tr>';
+					echo '			<th>Média</th>';
+					echo '			<th>Date de réservation</th>';
+					echo '			<th>Position dans la file</th>';
+					echo '		</tr>';
+					echo '	</thead>';
+					foreach($query as $row)
+					{
+						$query2 = $application->database->prepare('
+							SELECT COUNT(ID) + 1 AS position
+							FROM emprunts
+							WHERE mediaID = :media AND utilisateurID <> :user AND date_emprunt IS NULL AND date_reservation IS NOT NULL AND date_reservation < :date');
+
+						$query2->execute(array(
+							':media' => $row['ID'],
+							':user'  => $application->currentUser->getID(),
+							':date'  => $row['date_reservation']));
+
+						$data = $query2->fetch();
+
+						$dateReservation = DateTime::createFromFormat('Y-m-d', $row['date_reservation']);
+
+						echo '	<tr>';
+						echo '		<td><a href="media.php?id='.$row['ID'].'">'.$row['titre'].'</a></td>';
+						echo '		<td>'.$dateReservation->format('Y-m-d').'</td>';
+						echo '		<td>'.$data['position'].'</td>';
+						echo '		<td><a href="#" onclick="return confirm(\'Voulez-vous vraiment supprimer cet enregistrement?\');">Supprimer</a></td>';
+						echo '	</tr>';
+					}
+					echo '</table>';
+				}
+				?>
+
 			</div>
 		</div>
 		<?php require('sharedFiles/footer.inc.php'); ?>
