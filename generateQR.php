c30c358239c34d8e274d8aa566163687cad4d207
Marc-Andre Destrempes <xeph_555@hotmail.com>
2011-06-01 19:49:53 -0400

    Enlever les contours pour les table. Ajout de la page generateQR.php au menu administration. Corriger un bug ou pour mes reservations les requetes ne retourne pas toujours de resultats.

--- generateQR.php
+++ generateQR.php
@@ -1,3 +1,4 @@
+<?php require('sharedFiles/pageStart.inc.php'); ?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -10,93 +11,17 @@
 		<?php include('sharedFiles/javascript.inc.php'); ?>
 		<script src="javascript/jquery/QRCode.js"></script>
 		<script src="javascript/generateQRCode.js"></script>
+		<style type="text/css">div img, input.qr {display:block; margin:auto;}</style>
 	</head>
 	<body>
-		<header>
-			<div id="user-bar">
-				<a href="#">Inscription</a>
-				<a href="#">Connexion</a>
-			</div>
-			<h1><a href="index.html" title="Retour à l'accueil">Carcajou</a></h1>
-			<img alt="Logo du Cégep de Trois-Rivières" src="images/logo.png">
-			<form>
-				<input type="search" placeholder="Rechercher">
-				<button type="submit">Rechercher</button>
-			</form>
-			<nav>
-				<ul>
-					<li><a href="#">Lorem</a></li>
-					<li><a href="#">Ipsum</a></li>
-					<li><a href="#">Dolor</a></li>
-					<li><a href="#">Sit</a></li>
-					<li><a href="#">Amet</a></li>
-				</ul>
-			</nav>
-		</header>
+		<?php require('sharedFiles/header.inc.php'); ?>
 		<div id="content">
-			<div id="vertical-breadcrumb">
-				<h3>Affinez votre recherche</h3>
-				<h4>Lorem ipsum </h4>
-				<ul>
-					<li><a href="http://www.google.com" onclick="return confirm('Voulez-vous vraiment supprimer cet enregistrement?');">Lorem</a></li>
-					<li><a href="#">Ipsum</a></li>
-					<li><a href="#">Amet</a></li>
-				</ul>
-				<h4>Lorem ipsum </h4>
-				<ul>
-					<li><a href="#">Lorem</a></li>
-					<li><a href="#">Ipsum</a></li>
-					<li><a href="#">Sit</a></li>
-					<li><a href="#">Amet</a></li>
-				</ul>
-				<h4>Lorem ipsum </h4>
-				<ul>
-					<li><a href="#">Lorem</a></li>
-					<li><a href="#">Ipsum</a></li>
-					<li><a href="#">Dolor</a></li>
-					<li><a href="#">Sit</a></li>
-					<li><a href="#">Amet</a></li>
-				</ul>
-				<h4>Lorem ipsum </h4>
-				<ul>
-					<li><a href="#">Lorem</a></li>
-					<li><a href="#">Ipsum</a></li>
-					<li><a href="#">Dolor</a></li>
-					<li><a href="#">Sit</a></li>
-					<li><a href="#">Amet</a></li>
-					<li><a href="#">Consectetur</a></li>
-					<li><a href="#">Lorem</a></li>
-					<li><a href="#">Ipsum</a></li>
-					<li><a href="#">Dolor</a></li>
-					<li><a href="#">Sit</a></li>
-					<li><a href="#">Amet</a></li>
-					<li><a href="#">Consectetur</a></li>
-				</ul>
-			</div>
-			<div id="search-results">
+			<div>
 				<form id="media" action="#" method="get">					
 				</form>
 			</div>
 		</div>
-		<footer>
-			<p>Travail fait par Marin Desharnais, Samuel Milette-Lacombe et Marc-André Destrempe.</p>
-			<nav>
-				<ul>
-					<li><a href="#">Lorem</a></li>
-					<li><a href="#">Ipsum</a></li>
-					<li><a href="#">Dolor</a></li>
-					<li><a href="#">Sit</a></li>
-					<li><a href="#">Amet</a></li>
-				</ul>
-				<ul>
-					<li><a href="#">Lorem</a></li>
-					<li><a href="#">Ipsum</a></li>
-					<li><a href="#">Dolor</a></li>
-					<li><a href="#">Sit</a></li>
-					<li><a href="#">Amet</a></li>
-				</ul>
-			</nav>
-		</footer>
+		<?php require('sharedFiles/footer.inc.php'); ?>
 	</body>
 </html>
 
--- php/myReservations.php
+++ php/myReservations.php
@@ -5,21 +5,21 @@ if (is_null($r)) {
 	echo "Vous devez être connecté.";
 }
 if ($r != Null) {
-	$sql="SELECT medias.ID as ID, medias.reference as reference, medias.titre as titre, medias.annee_publication as annee_publication, medias.notes as notes, medias.image as image, maisons_edition.nom as menom, supports.nom as cnom, emprunts.date_reservation as date_reservation FROM medias 
+	$sql="SELECT medias.ID as ID, medias.reference as reference, medias.titre as titre, medias.annee_publication as annee_publication, medias.notes as notes, medias.image as image, maisons_edition.nom as menom, supports.nom as cnom, emprunts.date_reservation as date_reservation, date_voulue as date_voulue FROM medias 
 		INNER JOIN maisons_edition ON medias.maison_editionID = maisons_edition.ID 
 		INNER JOIN supports ON medias.supportID = supports.ID 
 		INNER JOIN emprunts ON medias.ID = emprunts.mediaID
 		INNER JOIN utilisateurs ON emprunts.utilisateurID = utilisateurs.ID
-		WHERE utilisateurs.ID = '".$r."' AND emprunts.date_retour = '0000-00-00' AND emprunts.date_emprunt = '0000-00-00'";
+		WHERE utilisateurs.ID = '".$r."' AND emprunts.date_retour IS NULL AND emprunts.date_emprunt IS NULL";
 
 	$query = $application->database->prepare($sql);
 	$query->execute();
-
+	
 	foreach($query as $row)
 	{
 		echo "<div id='outerDiv' class='autosize'>";
 		echo "<div id='" . $row['reference'] . "'>";
-		echo "<a href='media.php?ID=" . $row['ID'] . "'><p id='" . $row['reference'] . "aazz'>" . $row['reference'] . " | " . $row['titre'] . " reserve pour le " . $row['date_reservation'] . "</p></a>";
+		echo "<a href='media.php?ID=" . $row['ID'] . "'><p id='" . $row['reference'] . "aazz'>" . $row['reference'] . " | " . $row['titre'] . " réservé pour le " . $row['date_voulue'] . " le " . $row['date_reservation'] . "</p></a>";
 		echo "<div class='tooltip'><img src='images/medias/" . $row['image'] . "' style='float:left;margin:0 15px 20px 0' height='75' weight='75' /><table style='margin:0'><tr><td>Titre : </td><td>" . $row['titre'] . "</td></tr><tr><td>Annee de publication : </td><td>" . $row['annee_publication'] . "</td></tr><tr><td>Maison d'edition : </td><td>" . $row['menom'] . "</td></tr><tr><td>Support : </td><td>" . $row['cnom'] . "</td></tr>		</table></div>";
 		echo "<script>$('#" . $row['reference'] . "aazz').tooltip({effect: 'slide'});</script>";
 		echo "</div>";
