cf1dcfd465d68897e29c2a1ea65f880169973d8c
Marc-Andre Destrempes <xeph_555@hotmail.com>
2011-06-02 10:30:02 -0400

    Ajout de commentaires dans les fichiers php.

--- php/getMedia.php
+++ php/getMedia.php
@@ -1,20 +1,23 @@
 <?php
 require_once('Application.class.php');
-$q = $_GET["q"];
+$q = $_GET["q"]; // represente le numero de reference du media
 if ((strlen($q) == 0) || ($q == Null)) {
 	$q = Null;
 }
 
 if ($q != Null) {
 
+	// la requete sql selectionne un sommaire des informations d'un media selon son numero de reference
 	$sql="SELECT medias.reference as reference, medias.titre as titre, medias.annee_publication as annee_publication, medias.notes as notes, medias.image as image, maisons_edition.nom as menom, supports.nom as cnom FROM medias 
 		INNER JOIN maisons_edition ON medias.maison_editionID = maisons_edition.ID 
 		INNER JOIN supports ON medias.supportID = supports.ID 
 		WHERE reference = '".$q."'";
 	
-		$query = $application->database->prepare($sql);
-		$query->execute();
+	$query = $application->database->prepare($sql);
+	$query->execute();
 
+	// on met le media dans un div qui s'adapte a son contenu, par la suite on cree un div pour lui associer un tooltip
+	// le tooltip contient un sommaire des informations d'un media que nous avons recu par la requete sql
 	foreach($query as $row)
 	{
 		echo "<div id='outerDiv' class='autosize'>";
