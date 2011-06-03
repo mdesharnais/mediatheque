9129e60965cfda0a0a6c959776c933e51e2db178
Marc-Andre Destrempes <xeph_555@hotmail.com>
2011-06-02 21:23:59 -0400

    Ajout de l'affichage complet d'un media, comme dans une recherche, dans la page de reservation.

--- makeReservation.php
+++ makeReservation.php
@@ -8,8 +8,10 @@
 		<title>Réservation - Médiatech du département de musique du cégep de Trois-Rivières</title>
 		<?php include('sharedFiles/style.inc.php'); ?>
 		<?php include('sharedFiles/javascript.inc.php'); ?>
+		<?php require('php/reservationMedia.php'); ?>
 		<style>.tooltip{display:none;background:url(http://static.flowplayer.org/tools/img/tooltip/black_arrow_big.png);height:163px;padding:40px 30px 10px 30px;width:310px;font-size:11px;color:#fff;}#outerDiv{min-width: 200px;}div.autosize{display:table;height:1px;}div.autosize>div{display:table-cell;}</style>
 		<script src='http://cdn.jquerytools.org/1.2.5/full/jquery.tools.min.js'></script>
+	<link rel="stylesheet" href="css/search-result.css">
 	</head>
 	<body>
 		<?php require('sharedFiles/header.inc.php'); ?>
@@ -28,9 +30,11 @@
 		
 							if(!$application->currentUser->haveRights('makeReservation', $application->rights['execute']))
 								throw new Exception('Vous n\'avez pas les droits nécessaires.');
-
+								
+							printSearchResults(createFromWhereClause($_GET['id']));
 							echo "<form id='reservation' Action='php/submitReservation.php'>";
 							echo "<script src='javascript/generateReservation.js' ></script>";
+							echo "<br>";
 							include('php/setDateList.php');
 							echo "<input type='submit' Value='Enregistrer'>";
 							echo "</form>";
