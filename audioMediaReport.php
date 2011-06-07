f3faed03988790a14e74623dc571bed439cfb9e5
Marc-Andre Destrempes <xeph_555@hotmail.com>
2011-06-06 20:00:01 -0400

    Creation du tableau pour les rapports. Ne reste que quelques colonnes a ajouter peut-etre.

--- audioMediaReport.php
+++ audioMediaReport.php
@@ -40,6 +40,40 @@
 					echo '</form>';
 				
 					// if param exist generate report else do nothing
+					if (isset($_GET['genreID']))
+					{
+					//catalogue, no de catalogue, titre, compositeur, interprète, titre de l'album/notes, référence
+					//RESTE CATALOGUE, NO CATALOGUE ET INTERPRETE A METTRE
+						$sql="SELECT exemplaires_medias.reference as reference, medias.titre as titre, medias.annee_publication as annee_publication, medias.notes as notes, maisons_edition.nom as menom, supports.nom as cnom FROM medias 
+							INNER JOIN maisons_edition ON medias.maison_editionID = maisons_edition.ID 
+							INNER JOIN supports ON medias.supportID = supports.ID 
+							INNER JOIN exemplaires_medias ON medias.ID = exemplaires_medias.exID
+							WHERE medias.genreID = '".$_GET['genreID']."' ORDER BY 1,2,3";
+						$query = $application->database->prepare($sql);
+						$query->execute();
+						
+						echo '<table>';
+						echo '<tr>';
+						echo '<th>Référence</th>';
+						echo '<th>Titre</th>';
+						echo '<th>Année de publication</th>';
+						echo '<th>Maison d\'édition</th>';
+						echo '<th>Support</th>';
+						echo '<th>Notes</th>';
+						echo '</tr>';
+						foreach($query as $row)
+						{
+							echo '<tr>';
+							echo '<td>'.$row['reference'].'</td>';
+							echo '<td>'.$row['titre'].'</td>';
+							echo '<td>'.$row['annee_publication'].'</td>';
+							echo '<td>'.$row['menom'].'</td>';
+							echo '<td>'.$row['cnom'].'</td>';
+							echo '<td>'.$row['notes'].'</td>';
+							echo '</tr>';
+						}
+						echo '</table>';
+					}
 				}
 			?>
 			</div>
